9c1e3a9e3b0e33eedd6041a914f956f532f05674
Anderson Mazzotti <anderson.mazzotti@alunos.sc.senac.br>
2022-09-19 19:37:42 -0300

    Primeira composicao

---
+++ src/composicao/carro.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composicao;
+
+/**
+ *
+ * @author anderson.mazzotti
+ */
+public class carro {
+
+    private motor motor;
+    private direcao direcao;
+
+    /**
+     * @return the motor
+     */
+    public motor getMotor() {
+        return motor;
+    }
+
+    /**
+     * @param motor the motor to set
+     */
+    public void setMotor(motor motor) {
+        this.motor = motor;
+    }
+
+    /**
+     * @return the direcao
+     */
+    public direcao getDirecao() {
+        return direcao;
+    }
+
+    /**
+     * @param direcao the direcao to set
+     */
+    public void setDirecao(direcao direcao) {
+        this.direcao = direcao;
+    }
+
+    
+    
+
+    
+}
---
+++ src/composicao/motor.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composicao;
+
+/**
+ *
+ * @author anderson.mazzotti
+ */
+public class motor {
+    
+    
+    private int potencia;
+
+    /**
+     * @return the potencia
+     */
+    public int getPotencia() {
+        return potencia;
+    }
+
+    /**
+     * @param potencia the potencia to set
+     */
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+    
+    
+    
+}
